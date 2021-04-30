7fb13a56294f4b1d22d1730aa78e4d1374d0f9ca
Richard <richard.yang.tong@gmail.com>
2021-04-29 20:39:14 -0400

    Added a case for white space.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,12 +3,8 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        // In the following code that I will type below, write down
-        // what parts of the code are expressions.
-        int score = 100; // Expression
-        if (score > 99) // Expression {
-            System.out.println("You got the high score"); // Expression
-            score = 0; // Expression
-        }
+        System.out.println("This is " +
+                "another " +
+                "still more.");
     }
 }
