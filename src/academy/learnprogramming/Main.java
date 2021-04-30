35d30b467723d5eda1b02e3bd6b5fbfdefe4e1da
Richard <richard.yang.tong@gmail.com>
2021-04-29 20:29:56 -0400

    Added solution.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         // In the following code that I will type below, write down
         // what parts of the code are expressions.
-        int score = 100;
-        if (score > 99) {
-            System.out.println("You got the high score");
-            score = 0;
+        int score = 100; // Expression
+        if (score > 99) // Expression {
+            System.out.println("You got the high score"); // Expression
+            score = 0; // Expression
         }
     }
 }
